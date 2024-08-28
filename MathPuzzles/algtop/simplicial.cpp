18a056741fda967e42628d383a863b365c26d472
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-08-28 11:51:47 -0400

    homology

--- MathPuzzles/algtop/simplicial.cpp
+++ MathPuzzles/algtop/simplicial.cpp
@@ -66,26 +66,69 @@ struct Simplicial
         return mtx;
     }
 
-    Eigen::MatrixXf BoundaryTwo()
+    Eigen::MatrixXf boundaryTwo()
     {
         int R = line.size();
-        int C;
-        if (triangle.has_value())
-        {
-            C = triangle->size();
-        }
+        int C = triangle.has_value() ? triangle->size() : 0;
         Eigen::MatrixXf mtx(R, C);
         mtx.setZero();
+        if (C != 0)
+        {
+            for (int j = 0; j < C; ++j)
+            {
+                const auto &[a, b, c] = (*triangle)[j];
+                std::vector<std::pair<int, int>> edges = {{a, b}, {b, c}, {a, c}};
+
+                for (int i = 0; i < R; ++i)
+                {
+                    const auto &line_pair = line[i];
+
+                    if (line_pair == edges[0])
+                    {
+                        mtx(i, j) = 1;
+                    }
+                    else if (line_pair == edges[1])
+                    {
+                        mtx(i, j) = 1;
+                    }
+                    else if (line_pair == edges[2])
+                    {
+                        mtx(i, j) = -1;
+                    }
+                }
+            }
+        }
 
         return mtx;
     }
 
-    void homologyZero()
+    int homologyZero()
     {
         Eigen::MatrixXf mtx = boundaryOne();
         Eigen::FullPivLU<Eigen::MatrixXf> lu_decomp(mtx);
         int rank = lu_decomp.rank();
-        std::cout << (point.size() - rank) << "\n";
+        return (point.size() - rank);
+    }
+
+    int homologyOne()
+    {
+        // boundaries
+        Eigen::MatrixXf d1 = boundaryOne();
+        Eigen::MatrixXf d2 = boundaryTwo();
+        int col_d1 = line.size();
+
+        // rank
+        Eigen::FullPivLU<Eigen::MatrixXf> lu_decompd1(d1);
+        int rank_d1 = lu_decompd1.rank();
+        int kernel = col_d1 - rank_d1;
+        if (d2.rows() == 0 || d2.cols() == 0)
+        {
+            return kernel;
+        }
+        Eigen::FullPivLU<Eigen::MatrixXf> lu_decompd2(d2);
+        int rank_d2 = lu_decompd2.rank();
+
+        return kernel - (rank_d2);
     }
 
     // Second homology group
@@ -93,12 +136,26 @@ struct Simplicial
 
 int main()
 {
-    Point myPoint{1, 2, 3, 4};
-    Line myLine{{1, 3}, {1, 2}, {2, 3}, {2, 4}, {3, 4}};
-    Triangle myTriangle = {{1, 2, 3}};
-    Simplicial torus(myPoint, myLine, myTriangle, std::nullopt);
+    Point myPoint{1, 2, 3};
+    Line myLine{{1, 2}, {2, 3}, {1, 3}};
+    Triangle myTriangle{{1, 2, 3}};
+    Simplicial circle(myPoint, myLine, myTriangle, std::nullopt);
+    std::cout << circle.homologyOne();
     return 0;
 }
+
+//[1,2,3]
+
+//[1,2],[2,3],[1,3]
+//    1     1   -1
+
+/*
+      [1,2,3] []
+[1,2]   1
+[2,3]   1
+[1,3]   -1
+
+*/
 
 // G
 
