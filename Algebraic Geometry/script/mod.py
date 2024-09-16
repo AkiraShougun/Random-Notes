a99e3c9e95478fa3a6a3dc733b64871726fecc32
AkiraShougun <153398075+AkiraShougun@users.noreply.github.com>
2024-09-16 16:20:13 -0400

    py

--- Algebraic Geometry/script/mod.py
+++ Algebraic Geometry/script/mod.py
@@ -6,15 +6,3 @@ for i in range(3,12):
     print(Cohomology(i))    
 
 #Write a print statement to print a hatsune miku ascii art in the console
-
-text = """
-            .--.
-            |o_o |
-            |:_/ |
-             //   \ \\
-            (|     | )
-             /'\_   _/`\\
-                \___)=(___/
-    """ 
-
-print(text)
